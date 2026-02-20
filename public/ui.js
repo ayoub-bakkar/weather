af78532138a65f18941f20fbc96ea528658ce14e
ayoub-bakkar <ayoub.bakkar.dev@gmail.com>
2026-02-20 18:15:52 +0300

    Improve the design

--- public/ui.js
+++ public/ui.js
@@ -6,21 +6,24 @@ const detailsConfig = [
     unit: "°C",
   },
   { label: "Humidity", icon: "fa-solid fa-eye", key: "humidity", unit: "%" },
-  { label: "Visibility", icon: "eye.png", key: "visibility", unit: "km" },
-  { label: "Wind Speed", icon: "wind.png", key: "speedWind", unit: "km/h" },
-  { label: "Clouds", icon: "clouds.png", key: "clouds", unit: "%" },
+  { label: "Visibility", icon: "fa-regular fa-eye", key: "visibility", unit: "km" },
+  { label: "Wind Speed", icon: "fa-solid fa-wind", key: "speedWind", unit: "km/h" },
+  { label: "Clouds", icon: "fa-solid fa-cloud", key: "clouds", unit: "%" },
 ];
+
+//<i class="fa-solid fa-cloud"></i>
 function renderDetails(res) {
   const container = document.getElementById("details-container");
 
   container.innerHTML = detailsConfig.map(
     (config) => `
     <div class="detail-card">
-    <div class="info">
+      <div class="info">
+        <i class="${config.icon}"></i>
         <span>${config.label}</span>
         <h4>${res[config.key]}${config.unit}</h4>
-    </div>
+      </div>
     </div>
     `,
-  ).join();
+  ).join('');
 }
