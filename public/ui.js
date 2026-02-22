bef50a68cc79df8deb8c6f185ab07f0fa870d393
ayoub-bakkar <ayoub.bakkar.dev@gmail.com>
2026-02-23 00:43:59 +0300

    Add a function to display weather conditions using the ID.

--- public/ui.js
+++ public/ui.js
@@ -1,9 +1,11 @@
-const infoWeather = document.querySelectorAll("[data-key]");
+const city = document.querySelector(".city");
+const country = document.querySelector(".country");
 const temp = document.querySelector(".temp");
+const image = document.querySelector(".image");
+const infoWeather = document.querySelectorAll("[data-key]");
 const description = document.querySelector(".description");
-const city = document.querySelector(".city")
-const country = document.querySelector(".country")
 const time = document.getElementById("time");
+
 const units = {
   humidity: "%",
   speedWind: "km/h",
@@ -13,40 +15,40 @@ const units = {
 };
 
 const updateWeatherUI = (data) => {
-  renderCountryCity (data);
+  renderCountryCity(data);
   renderMainWeather(data);
   displayExtraInfo(data);
-  renderTime(data)
-  //renderWeatherImage (data)
-}
+  renderTime(data);
+  renderWeatherImage(data);
+};
 const renderTime = (timeِAPI) => {
-  const localTimeMs  = (timeِAPI.dt + timeِAPI.timezone) * 1000;
-  const  date = new Date(localTimeMs)
-  const timeString = date.toLocaleTimeString('en-US', {
-    hour: '2-digit',
-    minute: '2-digit',
+  const localTimeMs = (timeِAPI.dt + timeِAPI.timezone) * 1000;
+  const date = new Date(localTimeMs);
+  const timeString = date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
     hour12: true,
-    timeZone: 'UTC'
+    timeZone: "UTC",
   });
   time.textContent = timeString;
-}
-   
+};
+
 const renderMainWeather = (main) => {
   if (main.temp !== undefined) {
-    temp.innerHTML = `${main.temp}<sup class="unit">c</sup>`;
+    temp.innerHTML = `${Math.floor(main.temp)}<sup class="unit">c</sup>`;
   }
   if (main.description !== undefined) {
     description.textContent = main.description;
   }
 };
 const renderCountryCity = (CountryCity) => {
-  if(CountryCity.country !== undefined) {
+  if (CountryCity.country !== undefined) {
     country.textContent = CountryCity.country;
   }
-  if(CountryCity.country !== undefined) {
+  if (CountryCity.country !== undefined) {
     city.textContent = CountryCity.city;
   }
-}
+};
 const displayExtraInfo = (extraInfo) => {
   infoWeather.forEach((el) => {
     const key = el.dataset.key;
@@ -57,7 +59,35 @@ const displayExtraInfo = (extraInfo) => {
   });
 };
 
-const renderWeatherImage = (image) => {
-    
-}
-export {updateWeatherUI};
+const renderWeatherImage = (weatherConditionCodes) => {
+  const id = weatherConditionCodes.id;
+  console.log(id);
+  if (id >= 200 && id <= 232) {
+    image.src = "image/thunderstorm.png";
+  }
+  if (id >= 300 && id <= 321) {
+    image.src = "image/rain.png";
+  }
+  if (id >= 600 && id <= 622) {
+    image.src = "image/snow.png";
+  }
+  if (id == 800) {
+    image.src = "image/clear-sky.png";
+  }
+  if (id >= 701 && id <= 781) {
+    image.src = "image/mist.png";
+  }
+  if (id == 801) {
+    image.src = "image/few-clouds.png";
+  }
+  if (id == 802) {
+    image.src = "image/scattered-clouds.png";
+  }
+  if (id == 803) {
+    image.src = "image/broken-clouds.png";
+  }
+  if (id == 804) {
+    image.src = "image/broken-clouds.png";
+  }
+};
+export { updateWeatherUI };
